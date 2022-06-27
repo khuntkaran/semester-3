d35a1b4ebd099675fc11ebd5f1c7723cd063ff91
khuntkaran <21010101108@darshan.ac.in>
2022-06-27 21:33:24 +0530

    .

--- Data Structure/01-simple program/Prime.java
+++ Data Structure/01-simple program/Prime.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
 public class Prime {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        checkNumber(sc.nextInt());
+    }
+    static void checkNumber(int n){
+        int flag =0;
+        for(int i=2;i<(n/2);i++){
+            if(n%i==0){
+                flag =1;
+                break;
+            }
+        }
+        if(flag==1){
+            System.out.println("number is not prime");
+        }
+        else{
+            System.out.println("number is prime");
+        }
+    }
 }

6bc436b75e75c31150e3181897bfe4f5ae7680c8
khuntkaran <21010101108@darshan.ac.in>
2022-07-12 11:42:52 +0530

    .

--- Data Structure/01-Method/E04_Armstrong.java
+++ Data Structure/01-Method/E04_Armstrong.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class E04_Armstrong {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter n : ");
+        int n = sc.nextInt();
+        System.out.print("Armstrong : ");
+        for(int i=1;i<=n;i++){
+            int c =i,k=0,check=0,temp=1;
+
+            int s= (String.valueOf(i)).length();
+
+            while (c!=0){
+                k= c %10;
+               check = (int) (check+ Math.pow(k,s));
+               c=c/10;
+            }
+            if(i == check){
+                System.out.print(i+" ");
+            }
+        }
     }
 }

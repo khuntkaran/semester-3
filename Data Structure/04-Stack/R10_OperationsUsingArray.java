a3e216ad184d2dee2f6d76226349cf5cd0deb765
khuntkaran <21010101108@darshan.ac.in>
2022-07-13 22:31:24 +0530

    .

--- Data Structure/04-Stack/R10_OperationsUsingArray.java
+++ Data Structure/04-Stack/R10_OperationsUsingArray.java
@@ -4,41 +4,34 @@ public class R10_OperationsUsingArray {
     int[] stack = new int[10];
     int Top = -1;
     static Scanner sc = new Scanner(System.in);
-    void push(){
-        Top++;
-        if(Top>9){
-            Top--;
+    void push(int x){
+        if(Top>=9){
             System.out.println("stack overflow");
             return;
         }
-        System.out.print("enter element : ");
-        stack[Top] = sc.nextInt();
+        Top++;
+        stack[Top] = x;
     }
-    void pop(){
-        Top--;
+    int pop(){
         if(Top<0){
-            System.out.println("stack underflow");
-            Top++;
+            System.out.print("stack underflow ");
+            return 0;
         }
+        Top--;
+        return stack[Top+1];
     }
-    void peep(){
-        System.out.print("which element check from Top in stack : ");
-        int i = sc.nextInt();
+    int peep(int i){
+        if(Top-i+1<0){
+            System.out.println("stack underflow");
+            return 0;
+        }
+        return stack[Top-i+1];
+    }
+    void change(int x,int i){
         if(Top-i+1<0){
             System.out.println("stack underflow");
             return;
         }
-        System.out.println(i+" element from Top in stack is "+stack[Top-i+1]);
-    }
-    void change(){
-        System.out.print("which element change from Top in stack : ");
-        int i = sc.nextInt();
-        if(Top-i+1<0){
-            System.out.println("stack underflow");
-            return;
-        }
-        System.out.print("new element is : ");
-        int x = sc.nextInt();
         stack[Top-i+1] = x;
     }
     void display(){
@@ -51,34 +44,35 @@ public class R10_OperationsUsingArray {
     public static void main(String[] args) {
 
         R10_OperationsUsingArray oua = new R10_OperationsUsingArray();
-        boolean b = true;
-        while (b){
-            System.out.println("1 : push element in stack");
-            System.out.println("2 : pop element in stack");
-            System.out.println("3 : check element in stack");
-            System.out.println("4 : change element in stack");
-            System.out.println("5 : display stack");
-            System.out.println("6 : exit");
-            System.out.print("operation : ");
+        boolean flag = true;
+        while (flag){
+            System.out.print("1 : push element in stack\n2 : pop element in stack\n3 : check element in stack\n4 : change element in stack\n5 : display stack\n6 : exit\noperation : ");
             int y = sc.nextInt();
             switch (y){
                 case 1 :
-                    oua.push();
+                    System.out.print("enter element : ");
+                    oua.push(sc.nextInt());
                     break;
                 case 2 :
-                    oua.pop();
+                    System.out.println("remove last element : "+oua.pop());
                     break;
                 case 3 :
-                    oua.peep();
+                    System.out.print("which element check from Top in stack : ");
+                    int i = sc.nextInt();
+                    System.out.println(i+" element from Top in stack is "+oua.peep(i));
                     break;
                 case 4 :
-                    oua.change();
+                    System.out.print("which element change from Top in stack : ");
+                    int i2 = sc.nextInt();
+                    System.out.print("new element is : ");
+                    int x = sc.nextInt();
+                    oua.change(x,i2);
                     break;
                 case 5 :
                     oua.display();
                     break;
                 case 6 :
-                    b = false;
+                    flag = false;
                     break;
                 default:
                     System.out.println("your choice invalid");

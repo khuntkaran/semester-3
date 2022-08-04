2b49fdd1360bb434e9dd9d9200763556343bc2fc
khuntkaran <21010101108@darshan.ac.in>
2022-08-04 21:59:13 +0530

    .

--- Data Structure/06_Linked_List/01_Singly_Linked_List/R17_Create_Node.java
+++ Data Structure/06_Linked_List/01_Singly_Linked_List/R17_Create_Node.java
@@ -3,15 +3,10 @@ import java.util.Scanner;
 class New_Node{
     Object value;
     New_Node address;
-    void display(){
-
-    }
 }
 public class R17_Create_Node{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        New_Node N1 = new New_Node();
-        boolean flag=true;
         int n=6;
         New_Node[] NN=new New_Node[n];
         for(int i=0;i<n;i++){
@@ -21,12 +16,18 @@ public class R17_Create_Node{
             }
             System.out.print("Enter element : ");
             NN[i].value=sc.next();
+
             System.out.println("add element(y/n)");
-            
             char temp = sc.next().charAt(0);
-            if(temp=='n'){
+            if(temp=='n')
                 break;
-            }
+
+        }
+        New_Node nw= NN[0];
+        while(nw!=null){
+            Object element = nw.value;
+            System.out.println(element);
+             nw= nw.address;
         }
     }
 }

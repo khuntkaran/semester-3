f24c25f5408731e1e15719e8e3b7ab40fb9b19b2
khuntkaran <21010101108@darshan.ac.in>
2022-08-12 05:38:47 +0530

    .

--- DBMS/Trigger_Cursor_Ecxeption.sql
+++ DBMS/Trigger_Cursor_Ecxeption.sql
@@ -10,10 +10,13 @@ create table demotable(
 
 
 
-insert into maintable values('01')
+insert into maintable values('04')
 update maintable set invaluse='001' where invaluse='01'
 delete from maintable where invaluse='001'
 
+
+
+-------- Trigger-------------
 
 create trigger mainmassege
 on maintable
@@ -64,3 +67,61 @@ select * from maintable
 
 delete from demotable
 delete from maintable
+
+
+---------- Cursor-----------
+
+declare 
+	@invalue	varchar(50);
+declare cursor_demo cursor
+for select
+		invaluse
+	from
+		maintable;
+open cursor_demo;
+fetch next from cursor_demo into
+	@invalue;
+while @@FETCH_STATUS = 0
+	begin
+		insert into demotable values('cursor : '+@invalue)
+		fetch next from cursor_demo into
+		@invalue;
+	end
+close cursor_demo;
+deallocate cursor_demo;
+
+
+
+--------Exception Handling--------
+
+CREATE PROC PR_divide
+@a decimal,
+@b decimal,
+@c decimal output
+AS
+BEGIN
+BEGIN TRY
+SET @c = @a / @b;
+END TRY
+BEGIN CATCH
+SELECT
+ERROR_NUMBER() AS ErrorNumber
+,ERROR_SEVERITY() AS ErrorSeverity
+,ERROR_STATE() AS ErrorState
+,ERROR_PROCEDURE() AS ErrorProcedure
+,ERROR_LINE() AS ErrorLine
+,ERROR_MESSAGE() AS ErrorMessage;
+END CATCH
+END;
+
+--Executing a procedure
+DECLARE @r decimal;
+EXEC PR_divide 10, 2, @r output;
+PRINT @r;
+--Output
+5
+
+--Executing a procedure
+DECLARE @r decimal;
+EXEC PR_divide 10, 0, @r output;
+PRINT @r;

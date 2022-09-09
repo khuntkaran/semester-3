2c622826d2cd12b9bcee07faa075c287119fe208
khuntkaran <21010101108@darshan.ac.in>
2022-09-09 18:53:13 +0530

    .

--- DBMS/06_Cursor_Exception-Handling.sql
+++ DBMS/06_Cursor_Exception-Handling.sql
@@ -77,7 +77,7 @@ begin
 		throw 50005,'error no plogid with this id',1
 end
 
-execute PR_if_exist_PersonLog2 1
+execute PR_if_exist_PersonLog2 11
 
 
 
@@ -85,23 +85,64 @@ execute PR_if_exist_PersonLog2 1
 declare
 	@PersonLogID int,
 	@PersonName varchar(40);
-declare per_cursor3 cursor
+declare per_cursor cursor
 for select
-		PersonLogID	
+		PersonLogID,
 		PersonName
 	from 
 		PersonLog2;
-
-open per_cursor3
-fetch next from per_cursor3 into
+open per_cursor
+fetch next from per_cursor into
 	@PersonLogID,
 	@PersonName;
 while @@FETCH_STATUS=0
 	begin
 		print cast(@PersonLogID as varchar)+'-'+@PersonName;
-		fetch next from per_cursor3 into
+		fetch next from per_cursor into
 			@PersonLogID,
 			@PersonName;
 	end;
-close per_cursor2
-deallocate per_cursor2
+close per_cursor
+deallocate per_cursor
+
+
+
+----7
+create table Student
+(
+	id		int,
+	rno		int,
+	enroll	varchar(11),
+	name	varchar(50),
+	branch	varchar(50),
+	uni		varchar(50)
+)
+insert into Student values(1,101,'xyz','karan','cse','DU')
+insert into Student values(5,105,'xyz','kkk','ce','DU')
+insert into Student values(10,110,'xyz','khunt','ee','DU')
+
+declare
+	@rno int,
+	@branch varchar(50)
+declare update_Enroll_student cursor
+for select
+		rno,
+		branch
+	from
+		Student
+open update_Enroll_student
+fetch next from update_Enroll_student into
+	@rno,
+	@branch
+while @@FETCH_STATUS=0
+	begin
+		update Student set enroll='SOE22'+@branch+CONVERT(varchar,@rno) where rno=@rno
+		fetch next from update_Enroll_student into
+			@rno,
+			@branch
+	end
+close update_Enroll_student
+deallocate update_Enroll_student
+
+
+select * from Student
